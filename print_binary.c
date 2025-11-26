48db634a2078c6e09b5e57fa48fb6bfc888b96dd
Antgst <ant.gousset@gmail.com>
2025-11-26 19:07:08 +0100

    feat: add the file print_binary.c

--- print_binary.c
+++ print_binary.c
@@ -1,1 +1,13 @@
 #include "main.h"
+
+/**
+ * print_binary - placeholder for binary printing
+ * @args: list of arguments
+ *
+ * Return: 0 (nothing printed yet)
+ */
+int print_binary(va_list args)
+{
+	(void)args;
+	return (0);
+}
