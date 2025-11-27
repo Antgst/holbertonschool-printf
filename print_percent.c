19ed1ace7906476fe0264c3fa84471761ecfc803
Antgst <ant.gousset@gmail.com>
2025-11-27 13:32:28 +0100

    feat: add the print_percent function

---
+++ print_percent.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_percent: 
+* @args: unused parameters
+* 
+* Return: 1, the number of character printed
+*/
+
+ int print_percent(va_list args)
+ {
+    (void)args
+
+    _putchar('%');
+
+    return (1);
+ }
+ 
