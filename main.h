c422ca5567b5eca4d6e24f37ef209ef5871f5c16
Tamsir-ui <djibriltamsirn@gmail.com>
2025-11-27 09:43:01 +0100

    fix: add int print_unsigned

--- main.h
+++ main.h
@@ -22,6 +22,7 @@ int _printf(const char *format, ...);
 int print_char(va_list args);
 int print_string(va_list args);
 int print_int(va_list args);
+int print_unsigned(unsigned int n);
 int print_binary(va_list args);
 int (*get_print_func(char c))(va_list);
 
