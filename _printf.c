00ca02e9357d2d0bd4983e227b531efcb075db71
Antgst <ant.gousset@gmail.com>
2025-11-27 13:10:17 +0100

    fix: delete all the files with issues, redo all of them

--- _printf.c
+++ _printf.c
@@ -10,56 +10,5 @@
 
  int _printf(const char *format, ...)
  {
-    va_list args;
-    int i = 0;
-    int count = 0;
-    int (*f)(va_list);
-
-    if (format == NULL)
-    return (-1);
-
-va_start(args, format);
-
-while (format[i] != '\0')
-{
-    if (format[i] != '%')
-    {
-        _putchar(format[i]);
-        count++;
-    }
-    else
-    {
-        i++;
-        
-        if (format[i] == '\0')
-        {
-            va_end(args);
-            return (-1);
-        }
-
-        if (format[i] == '%')
-        {
-            _putchar('%');
-            count++;
-        }
-        else
-        {
-            f = get_print_func(format[i]);
-            if (f != NULL)
-            {
-                count += f(args);
-            }
-            else
-            {
-                _putchar('%');
-                _putchar(format[i]);
-                count += 2;
-            }
-        }
-    }
-    i++;
-}
-
-va_end(args);
-return (count);
-}
+    
+ }
